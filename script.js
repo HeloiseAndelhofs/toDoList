e9194d728c04c1b75f21a009822b3d00b66b817a
HeloiseAndelhofs <heloise.andelhofs@proximus.be>
2023-09-15 15:54:41 +0200

    6th step

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("bonjour");
+let task = document.getElementById("task");
+let addTask = document.getElementById("add-task");
+let taskList = document.getElementById("task-list");
+
+
+
+function addTasks(){
+    let taskValue = task.value;
+    taskList.innerHTML += `<li>
+    <p>${taskValue}</p>
+    <div>
+        <button>Done</button>
+        <button>Delete</button>
+    </div>
+</li>`
+task.value = "";
+};
+addTask.addEventListener("click", addTasks);
+
+function deleteTask(event){
+    const deleteTasks = event.target.closest("li");
+    deleteTasks.remove();
+}
+taskList.addEventListener("click", deleteTask);
