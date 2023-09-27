2f677662cbd3be00204f71e597d0a4425722294b
HeloiseAndelhofs <heloise.andelhofs@proximus.be>
2023-09-27 12:01:06 +0200

    done to undo, class completed, input required

--- script.js
+++ script.js
@@ -1,35 +1,48 @@
 let task = document.getElementById("task");
 let addTask = document.getElementById("add-task");
 let taskList = document.getElementById("task-list");
+const input = document.querySelector("input");
 
 
 function addTasks(){
     let taskValue = task.value;
-    taskList.innerHTML += `<li>
-    <p>${taskValue}</p>
-    <div>
-        <button class="done">Done</button>
-        <button class="delete">Delete</button>
-    </div>
-</li>`
-task.value = "";
+    if (input.value.length !== 0) {
+        taskList.innerHTML += `<li>
+        <p>${taskValue}</p>
+        <div>
+            <button class="done">Done</button>
+            <button class="delete">Delete</button>
+        </div>
+    </li>`
+    task.value = "";
+    } else {
+        alert("Veuillez renseigner une tâche.");
+    }
 };
 
 
 function deleteOrCompleteTask(event){
     const btnClickedClass = event.target.classList;
+    console.log(btnClickedClass);
     if (btnClickedClass[0] === "done") {
         btnClickedClass.remove("done");
         btnClickedClass.add("undo");
-        }
+        event.target.textContent = "Undo";
+        event.target.parentElement.previousElementSibling.classList.add("completed");
+    }
     else if (btnClickedClass[0] === "undo") {
         btnClickedClass.remove("undo");
         btnClickedClass.add("done");
-    
+        event.target.textContent = "Done";
+        event.target.parentElement.previousElementSibling.classList.remove("completed");
     }
-    else { 
-        const deleteTasks = event.target.closest("li");
-        deleteTasks.remove();
+    else if (btnClickedClass[0] === "delete") { 
+        if (confirm("Voulez vous supprimer cette tâche ?") === true) {
+            const deleteTasks = event.target.closest("li");
+            deleteTasks.remove();
+        } else {
+           
+        }
     }
 }
     addTask.addEventListener("click", addTasks);
